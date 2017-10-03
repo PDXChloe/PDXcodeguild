800f15dd147b532ec4847e1ab3eab207b9028023
Chloe Chatter <chloe_elliott@me.com>
2017-10-03 12:03:33 -0700

    python turtle star, initial commit

---
+++ Python/star.py
@@ -0,0 +1,17 @@
+from turtle import *
+
+fillcolor('yellow')
+begin_fill()
+
+forward(100)
+right(144)
+forward(100)
+right(144)
+forward(100)
+right(144)
+forward(100)
+right(144)
+forward(100)
+
+end_fill()
+done()
