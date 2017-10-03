80b99e7c3dc620ac3a5ea6a6b398370c2fefd673
Chloe Chatter <chloe_elliott@me.com>
2017-10-03 12:02:41 -0700

    made a stick figure in python turtle, initial commit

---
+++ Python/stickfigure.py
@@ -0,0 +1,17 @@
+from turtle import *
+
+pensize(3)
+
+circle(50)
+forward(100)
+back(200)
+forward(100)
+setheading(90)
+back(150)
+right(45)
+back(100)
+forward(100)
+left(90)
+back(100)
+
+done()
