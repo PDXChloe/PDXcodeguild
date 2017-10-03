9ce22c324a34fc4c100b2b6fbd3f36e303f1e087
Chloe Chatter <chloe_elliott@me.com>
2017-10-03 12:19:35 -0700

    added color

--- Python/stickfigure.py
+++ Python/stickfigure.py
@@ -1,8 +1,13 @@
 from turtle import *
 
 pensize(3)
+color("purple", "yellow")
+
+begin_fill()
 
 circle(50)
+
+end_fill()
 forward(100)
 back(200)
 forward(100)
@@ -13,5 +18,6 @@ back(100)
 forward(100)
 left(90)
 back(100)
+
 
 done()
