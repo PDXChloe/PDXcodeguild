b97ac041175023a91a760829e6816ab1a76951ba
Chloe Chatter <chloe_elliott@me.com>
2017-10-04 14:58:06 -0700

    paper, rock and scissor game complete

--- Python/lab08-rockpaperscissors.py
+++ Python/lab08-rockpaperscissors.py
@@ -32,5 +32,68 @@ scissors = scissors
 
 '''
 
-print("Welcome to the Paper, Rock, Scissors game!")
-player = input("Enter your choice either rock, paper, scissors")
+
+
+'''
+user_input = ''
+while user_input != 'done':
+    user_input = input('enter something, or \'done\' if done: ')
+    if user_input != 'done':
+        # do something with the input
+
+
+while True:
+    user_input = input('enter something, or \'done\' if done: ')
+    if user_input == 'done':
+        break
+    # do something with the input
+
+'''
+
+
+
+
+
+
+
+
+import random   #this lets you do random functions
+import time
+
+game_pieces = ['rock', 'paper', 'scissors']   #creating a list of choices for game play
+com_choice = random.choice(game_pieces)     #computer making a choice but, not showing yet.....
+
+print("")   #I just like white space....
+print("")
+
+print("Welcome to the Paper, Rock, Scissors game!\n")  #Welcome message
+time.sleep(1)
+name = input("What is your name? \n")
+player = input(name.title() + ", please enter your choice either rock, paper, scissors.\n")
+
+
+print(com_choice)
+
+if com_choice == player.lower():
+    print("Argh, we both win")
+
+if com_choice == game_pieces[0] and player.lower() == game_pieces[1]:
+    print("Paper covers rock, " + name.title() + " wins!")
+
+if com_choice == game_pieces[0] and player.lower() == game_pieces[2]:
+    print("Rock crushes scissors, you lose!")
+
+if com_choice == game_pieces[1] and player.lower() == game_pieces[0]:
+    print("Paper covers rock, you lose!")
+
+if com_choice == game_pieces[1] and player.lower() == game_pieces[2]:
+    print("Scissors cut paper " + name.title() + " wins!")
+
+if com_choice == game_pieces[2] and player.lower() == game_pieces[0]:
+    print("Rock crushes scissors " + name.title() + " wins!")
+
+if com_choice == game_pieces[2] and player.lower() == game_pieces[1].lower():
+    print("Scissors cut paper, you lose!")
+
+
+#work on how to make this ask if you want to play again
