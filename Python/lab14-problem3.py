e5d2179da348f01773c8ff3b172f45b52bcdcd13
Chloe Chatter <chloe_elliott@me.com>
2017-10-05 16:03:58 -0700

    initial commit

---
+++ Python/lab14-problem3.py
@@ -0,0 +1,10 @@
+
+
+
+def is_even(a):
+    if a%2 == 0:  #checking if there is a ratio - if no ratio, its even
+        print("Number is even.")
+    else:
+        print("Number is odd.")  #Just need ELSE statement, if it isn't even then it is odd.
+
+is_even(7)
