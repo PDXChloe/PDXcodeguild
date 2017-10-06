96be81e5278662d240ee92a53ddfc1bb7d73a1b9
Chloe Chatter <chloe_elliott@me.com>
2017-10-06 12:18:01 -0700

    initial commit

---
+++ Python/lab14-problem8.py
@@ -0,0 +1,13 @@
+
+# Problem 8
+#
+# Write a function that returns the reverse of a list.
+#
+# def reverse(nums):
+#
+
+def reverse(nums):
+    rev_list = nums[::-1]
+    print(rev_list)
+
+reverse([1,2,3,4,5,6])
