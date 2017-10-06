366d455b2ff1982660bdffb6fe286107b8da44b8
Chloe Chatter <chloe_elliott@me.com>
2017-10-06 10:19:03 -0700

    initial commit

---
+++ Python/lab14-problem5.py
@@ -0,0 +1,20 @@
+'''Problem 5
+
+Write a function that returns the maximum of 3 parameters.
+
+def maximum_of_three(a, b, c):
+
+maximum_of_three(5,6,2) will return 6
+
+maximum_of_three(-4,3,10) will return 10
+'''
+
+def max_of_three(a, b, c):
+
+
+    the_big = [a, b, c]
+    print(max(the_big))
+
+    return
+
+max_of_three(100, 90, 67)
