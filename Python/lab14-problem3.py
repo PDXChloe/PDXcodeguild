4f7bdfbd2d97d8de3fdfa02e9a47c914030686bd
Chloe Chatter <chloe_elliott@me.com>
2017-10-10 10:08:29 -0700

    added comments

--- Python/lab14-problem3.py
+++ Python/lab14-problem3.py
@@ -3,8 +3,18 @@
 
 def is_even(a):
     if a%2 == 0:  #checking if there is a ratio - if no ratio, its even
+    #return a/2 == a//2 possible solution
         print("Number is even.")
     else:
         print("Number is odd.")  #Just need ELSE statement, if it isn't even then it is odd.
 
 is_even(7)
+
+# matthew solution:
+
+# def is_even(a):
+#     return a%2 ==0
+#
+# def is_odd(a):
+#     return a%2 == 1
+#
