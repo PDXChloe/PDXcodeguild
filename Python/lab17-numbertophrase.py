3bf1ad666b2d607b9b678b8d85ab5021fb0996b9
Chloe Chatter <chloe_elliott@me.com>
2017-10-11 11:33:16 -0700

    initial commit

--- Python/lab17-numbertophrase.py
+++ Python/lab17-numbertophrase.py
@@ -13,26 +13,28 @@
 #
 # Handle numbers from 100-999.
 
+#This version is my first, I used a combo of dictionary and lists - is a no no.. lol but, interesting? No?
+
 dic_tens = {1: "ten" , 2: "twenty", 3: "thirty", 4: "fourty", 5: "fifty", 6: "sixty", 7: "seventy", 8: "eighty", 9: "ninety" }
 dic_ones = {1: "one", 2: "two", 3: "three", 4: "four", 5: "five", 6: "six", 7: "seven", 8: "eight", 9: "nine" }
 ones = ["zero","one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
 tens = ["ten","ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
-teens = ["n/a","eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
+teens = ["ten","eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
 
 
-user_num = int(input("Give me an one or two-digit number...>>>\n"))
+user_num = int(input("Give me an one or two-digit number...>>>\n")) #get user input, put in variable.
 
-tens_digit = user_num//10
+tens_digit = user_num//10 #finding the tens digit
 
-ones_digit = user_num%10
+ones_digit = user_num%10 #finding the ones digit
 
-if user_num < 10:
+if user_num < 10: #uses the list
     print(ones[user_num])
-elif user_num < 20 and user_num > 10:
+elif user_num < 20 and user_num > 10: #uses the list
     print(teens[user_num%10])
-elif user_num%10 == 0:
+elif user_num%10 == 0:  #uses the list
     print(tens[user_num//10])
-else:
+else:                                                           #uses the dictionary
     print(dic_tens[tens_digit] + "-" + dic_ones[ones_digit])
 
 
