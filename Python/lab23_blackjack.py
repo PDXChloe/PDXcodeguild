d19550aaa200f5f1a34503c680ff25f1794ace1b
Chloe Chatter <chloe_elliott@me.com>
2017-10-13 11:48:56 -0700

    initial commit

---
+++ Python/lab23_blackjack.py
@@ -0,0 +1,23 @@
+
+
+c_1 = input("What is your first card?>>> \n")
+c_2 = input("What is your second card?>>> \n")
+c_3 = input("What is your third card?>>> \n")
+
+card_value ={"A": 1, "2": 2, "3": 3, "4" : 4, "5" : 5, "6" : 6, "7": 7, "8" : 8, "9": 9, "10": 10, "J" : 10, "Q": 10, "K": 10}
+
+#sum up the user card value
+
+user_points = card_value.get(c_1) + card_value.get(c_2) + card_value.get(c_3)
+
+
+if user_points < 17:
+    print(str(user_points) + ", you should Hit!")
+elif user_points >= 17:
+    print(str(user_points) + ", Stay!")
+elif user_points == 21:
+    print(str(user_points) + ", Blackjack!")
+elif user_points > 21:
+    print(str(user_points) + ", Already Busted!")
+
+#hmmmm, how to solve the A problem...?
