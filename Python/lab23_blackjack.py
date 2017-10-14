5bc68ce2d1433229d7082eabf0cce5110cfda9b0
Chloe Chatter <chloe_elliott@me.com>
2017-10-13 20:44:30 -0700

    made changes to math for Ace sit.

--- Python/lab23_blackjack.py
+++ Python/lab23_blackjack.py
@@ -9,12 +9,12 @@ card_value ={"A": 1, "2": 2, "3": 3, "4" : 4, "5" : 5, "6" : 6, "7": 7, "8" : 8,
 #sum up the user card value
 
 user_points = card_value.get(c_1) + card_value.get(c_2) + card_value.get(c_3)
-if c_1 == "A" and user_points < 9:
+if c_1 == "A" and user_points <= 11: #already added the A value as 1, so instead of the total being <= 11, it's 1 less
     user_points += 10
-elif c_2 == "A" and user_points < 9:
+elif c_2 == "A" and user_points <= 11: #you would never have two A = 11 because that would bust you with two cards.
     user_points += 10
-elif c_3 == "A" and user_points < 9:
-    user_points += 10
+elif c_3 == "A" and user_points <= 11: #so the code only needs to check for at least one A, and that the total of all three cards (with A = 1) being <=11
+    user_points += 10 #add 10 points to total because you already have 1 added for the Ace. 10+1 = 11 yay!
 
 
 if user_points < 17:
