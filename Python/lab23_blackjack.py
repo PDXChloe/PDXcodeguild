a91eff617d847968cdbac2613eb699ccc7c30109
Chloe Chatter <chloe_elliott@me.com>
2017-10-13 20:32:00 -0700

    finished the with the Ace part of the solution

--- Python/lab23_blackjack.py
+++ Python/lab23_blackjack.py
@@ -9,6 +9,12 @@ card_value ={"A": 1, "2": 2, "3": 3, "4" : 4, "5" : 5, "6" : 6, "7": 7, "8" : 8,
 #sum up the user card value
 
 user_points = card_value.get(c_1) + card_value.get(c_2) + card_value.get(c_3)
+if c_1 == "A" and user_points < 9:
+    user_points += 10
+elif c_2 == "A" and user_points < 9:
+    user_points += 10
+elif c_3 == "A" and user_points < 9:
+    user_points += 10
 
 
 if user_points < 17:
