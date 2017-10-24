65c8e5002edc72711f663955ac381607e17f366f
Chloe Chatter <chloe_elliott@me.com>
2017-10-24 15:28:13 -0700

    initial commit

---
+++ Python/lab22_practiceproblems2.py
@@ -0,0 +1,24 @@
+
+
+#problem 2 Print out every other element of a list, first using a while loop, then using a for loop.
+
+nums = [0,1,2,3,4,5,6,7,8,9]
+print(nums[::2]) #we could do it like this with this built in function
+
+
+#while loop example
+
+
+#for loop example
+other_nums = []
+
+for num in nums:
+    if num%2 == 0:
+        other_nums.append(num)
+print(other_nums)
+
+
+# while i in range(len(nums)):
+#     if i%2 == 0:
+#         other_nums.append(num)
+# print(other_nums)
