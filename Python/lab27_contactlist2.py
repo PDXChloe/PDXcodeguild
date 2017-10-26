d3d7cee27e6350d9655ba2b364d85d9af766c445
Chloe Chatter <chloe_elliott@me.com>
2017-10-26 15:49:11 -0700

    added Edit and Delete functions

--- Python/lab27_contactlist2.py
+++ Python/lab27_contactlist2.py
@@ -22,41 +22,73 @@
 
 
 
+
+find_words = ["find", "f"]
 stop_words = ["stop", "quit", "no", "q", "n", "done", "exit", "end", "none"]
 do_words = ["create", "new", "enter contact", "enter", "start"]
 header = ["first name", "last name", "color", "fruit"]
-new_contact = [] #the list the user input is appended to during the loop
-contact_info = {} #individual dictionaries the new contact info is looped into.
+
+
 contacts = [] #the main list the individual dictionaries are stored in.
 
-while True:
-    command = input("What is your command?").lower()
-    if command in stop_words:
-        print("Goodbye")
-        break
-    elif command in do_words:
 
-        f_name = input("Enter your first name. >>> ")
-        new_contact.append(f_name)
-        l_name = input("Enter your last name. >>> ")
-        new_contact.append(l_name)
-        color = input("Enter your favorite color. >>> ")
-        new_contact.append(color)
-        fruit = input("Enter your favorite fruit. >>> ")
-        new_contact.append(fruit)
+
+
+while True:
+    command = input('What is your command? "find" or "f", "quit" or "q", "create" or "c", "delete" or "d".').lower()
+    if command == 'quit' or command == 'q':
+        print('Goodbye')
+        break
+
+    elif command == 'create' or command == 'c':
+        new_contact = []  # the list the user input is appended to during the loop
+        contact_info = {}  # individual dictionaries the new contact info is looped into.
 
         for i in range(len(header)):
-            contact_info[header[i]] = new_contact[i]
+            attr = input('Enter your ' + header[i] + '. >>>')
+            contact_info[header[i]] = attr
+            #contact_info[header[i]] = new_contact[i]
         contacts.append(contact_info)
         print(contacts)
-        #return contacts
+
+    elif command in find_words:
+        who_for = input('Enter the first name of the person you are looking for >>> ')
+        for i in range(len(contacts)):
+            if who_for == contacts[i]['first name']:
+                print('Its here: ' + str(contacts[i]))
+                while True:
+
+                    sub_command = input('Would you like to edit this contact, "Y" or "N"')
+                    if sub_command == 'Y' or sub_command == 'y':
+                        attribute = input('Enter edit: "first name", "last name", "color" or "fruit"?').lower()
+                        value = input('Change ' + attribute + ' to:  ')
+                        contacts[i][attribute] = value
+                        print(str(contacts[i]))
+
+
+                    if sub_command == 'N' or sub_command == 'n':
+                        print('Done editing ' + str(contacts[i]) + '.')
+                        break
+
+    elif command == 'delete' or command == 'd':
+        who_for = input('Enter the first name of the contact you wish to delete. >>> ')
+        for i in range(len(contacts)): #need to adjust the range here ???? - deleting the first contact causes errors
+            if who_for == contacts[i]['first name']:
+                assurance = input('Are you sure you want to permanently delete ' + str(
+                    contacts[i]['first name']) + ' from database? "Y" or "N"')
+                if assurance == "Y" or assurance == "y":
+                    del contacts[i]
+                    print('Contact has been deleted')
+                    print(contacts)
+                if assurance == 'N' or assurance == 'n':
+                    break
+
 
     else:
-        print("Command not recognized. Please enter 'create' or 'done'.").lower()
+        print('Command not recognized. Please enter "create" or "done".').lower()
 
-#find_words = ["retrive", "find", "get"]
-# new_contact = {}
-#     for i in range(len(header)):
-#         new_contact[header[i]] = contact[i]
-#     contacts.append(new_contact)
-# print(contacts)
+
+
+
+
+
