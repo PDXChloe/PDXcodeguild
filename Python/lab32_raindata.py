e39961a3e44e0227a7fe5475437ec91f91d63d50
Chloe Chatter <chloe_elliott@me.com>
2017-10-26 09:44:23 -0700

    added comments

--- Python/lab32_raindata.py
+++ Python/lab32_raindata.py
@@ -2,7 +2,7 @@ import random
 import datetime
 import matplotlib.pyplot as plt
 
-class RainDataRow:
+class RainDataRow: #made a class but.....didn't really have to
 
     def __init__(self, date, daily_total):
         self.date = date
@@ -15,12 +15,12 @@ class RainDataRow:
 data = []
 with open('sauvies_island.rain.txt', 'r') as file:
     lines = file.read().split('\n')
-    for i in range(4, len(lines)):
+    for i in range(4, len(lines)): #scrubing some data, taking out the top four lines
         data_str = lines[i].split()
-        if len(data_str) < 1 or data_str[1] == '-':
+        if len(data_str) < 1 or data_str[1] == '-': #getting rid of all the dashes
             continue
 
-        date = datetime.datetime.strptime(data_str[0], '%d-%b-%Y')
+        date = datetime.datetime.strptime(data_str[0], '%d-%b-%Y') #using datetime module to get usable dates
         daily_total = int(data_str[1])
         rain_data_row = RainDataRow(date, daily_total)
         data.append(rain_data_row)
@@ -46,7 +46,7 @@ def variance(data, av):
     var = total / (len(data))
     return var
 
-
+#graph your data
 av = average(data)
 print(av)
 print(variance(data, av))
@@ -54,13 +54,13 @@ print(variance(data, av))
 x_values = []
 y_values = []
 
-for data_row in data:
+for data_row in data: #graph it by the year
     if data_row.date.year == 2012:
         x_values.append(data_row.date)
         y_values.append(data_row.daily_total)
 
 
-plt.plot(x_values, y_values, 'ro')
+plt.plot(x_values, y_values, 'ro') #using matplotlib module
 
 
-plt.show()
+plt.show() #calling it1
