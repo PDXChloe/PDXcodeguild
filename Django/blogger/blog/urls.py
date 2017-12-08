23ca3dc245bf51be932790d03e3333d89c462180
Chloe Chatter <chloe_elliott@me.com>
2017-12-08 12:30:14 -0800

    added user_login, user_logout, and create comment views

--- Django/blogger/blog/urls.py
+++ Django/blogger/blog/urls.py
@@ -4,5 +4,9 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+    url(r'^register/$', views.register, name='register'),
+    url(r'^login/$', views.user_login, name='user_login'),
+    url(r'^logout/$', views.user_logout, name='user_logout')
 ]
